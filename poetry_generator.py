faf80215550d56392d7fabcccaf13aead5af13da
Matthew Todd <matcatprg@yahoo.com>
2012-05-24 10:24:35 -0700

    Reimplemented to be cleaner
    Changed the design so that we have a much more clean implementation.
    This one should allow for much easier addition of grammar categories.

--- poetry_generator.py
+++ poetry_generator.py
@@ -8,25 +8,135 @@ Ex:
 produces:
     he eyes blue her
 
-It would basically be the reverse of parsing with a CFG. I.e: A template where
-the words are replaced randomly. Of course we'd want to allow the user to set
-and see which seed was used so we could reproduce the poem.
+A template where the words are replaced randomly. Of course we'd want to allow
+the user to set and see which seed was used so we could reproduce the poem.
 
-Might be worthwhile to use a CFG for this.
+TODO: program options and config file?
 '''
 
-import parse
+import random
+import sys
+
+
+
+NO_ERROR = 0
+SYSTEM_ERROR = 1
+USER_ERROR = 2
+
+
+
+cannon_name = {}
+grammar_list = {}
+
+
+def get_cannonical_name(grammar_name):
+    '''
+    Retrieves the cannoncial name for the specified grammar name.
+    '''
+    try:
+        return cannon_name[grammar_name]
+    except KeyError:
+        print('ERROR: unknown grammar name: %s' % grammar_name)
+        sys.exit(USER_ERROR)
+
+
+def random_word(word):
+    '''
+    Gets a random word for the specified grammar name.
+
+    The grammar name doesn't have to be the cannonical version, it will be
+    cannonicalized as part of this function.
+    '''
+    cannon_name = get_cannonical_name(word)
+    try:
+        word_list = grammar_dict[cannon_name]
+    except KeyError:
+        print('ERROR: unkown grammar name: %s' % cannon_name)
+        sys.exit(USER_ERROR)
+
+    try:
+        return random.choice(word_list)
+    except IndexError:
+        print('ERROR: no replacement words for grammar name: %s' % cannon_name)
+        sys.exit(USER_ERROR)
+    
+
+def create_cannonical_name_dict(filename):
+    '''
+    Reads in the specified file and extracts the grammar name cannonicalization
+    data.
+
+    File should be of the form:
+        name -> cannonical_name
+        name -> cannonical_name
+    '''
+    cannonical_name = {}
+
+    with open(filename, 'r') as spec_file:
+        for line in spec_file:
+            if line.strip() != '':
+                split_list = line.split('->')
+                if len(split_list) != 2:
+                    print('ERROR: line %r malformed' % line)
+                    sys.exit(USER_ERROR)
+                key = split_list[0].strip()
+                cannon = split_list[1].strip()
+
+                cannonical_name[key] = cannon
+
+    return cannonical_name
+
+
+def create_grammar_dict(cannon_name_list):
+    '''
+    Creates the grammar -> word list dictionary.
+
+    Assumes the filenames for the wordlist. Should be cannonical_name.wordlist.
+    We can change this later to be configurable, but its fine for now.
+    '''
+    grammar_dict = {}
+    for cannon_name in cannon_name_list:
+        filename = '%s.wordlist' % cannon_name
+        word_list = create_word_list(filename)
+        grammar_dict[cannon_name] = word_list
+    return grammar_dict
+
+
+def create_word_list(filename):
+    '''
+    Returns a word list from the provided file.
+
+    Each word in the file must be on a separate line.
+
+    TODO: filename doesn't exist exception
+    '''
+    return [line.strip() for line in open(filename, 'r')]
+
 
 def main():
     '''
     '''
+    global cannon_name, grammar_dict
+
+    print('DEBUG: populating data')
+    cannon_name = create_cannonical_name_dict('cannonicalization.config')
+
+    print('DEBUG: cannon_name = %r' % cannon_name)
+
+    grammar_dict = create_grammar_dict(set(cannon_name.values()))
+
     template = input('template: ')
 
     print("DEBUG: template = %r" % template)
 
-    result = parse.parser.parse(template).strip()
+    output = ''
+    for line in template.splitlines():
+        for word in line.split():
+            output = '%s %s' % (output, random_word(word))
+        output = '%s\n' % output
+    output = output.strip()
 
-    print("DEBUG: result = %r" % result)
+    print("DEBUG: output = %r" % output)
 
 
 if __name__ == '__main__':
